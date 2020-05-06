7f56eacad29783760c2ee8561423f95b6aa2cb66
bugsanderrors <open.nishant@gmail.com>
2020-05-06 15:36:42 +0530

    Add Week 1 Question 6

---
+++ W1Q6.cpp
@@ -0,0 +1,40 @@
+/*
+    Majority Element
+
+    Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+    You may assume that the array is non-empty and the majority element always exist in the array.
+
+    Example 1:
+    Input: [3,2,3]
+    Output: 3
+
+    Example 2:
+    Input: [2,2,1,1,1,2,2]
+    Output: 2
+*/
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        
+        int x = n/2;
+        
+        map<int, int> m;
+        
+        for(int i=0; i<n; i++)
+            m[nums[i]]++;
+        
+        int ans = 0;
+        
+        for(auto it = m.begin(); it!=m.end(); it++)
+            if(it->second > x)
+            {
+                ans = it->first;
+                break;
+            }
+        
+        return ans;
+    }
+};
