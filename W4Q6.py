6aa81420bb1f8c63ab1f21cdbc69022ed039bf4e
bugsanderrors <open.nishant@gmail.com>
2020-05-27 16:46:43 +0530

    Add Week 4 Question 6

---
+++ W4Q6.py
@@ -0,0 +1,70 @@
+"""
+    Possible Bipartition
+
+    Given a set of N people (numbered 1, 2, ..., N), we would like to split everyone into two groups of any size.
+
+    Each person may dislike some other people, and they should not go into the same group. 
+
+    Formally, if dislikes[i] = [a, b], it means it is not allowed to put the people numbered a and b into the same group.
+
+    Return true if and only if it is possible to split everyone into two groups in this way.
+
+    Example 1:
+    Input: N = 4, dislikes = [[1,2],[1,3],[2,4]]
+    Output: true
+    Explanation: group1 [1,4], group2 [2,3]
+
+    Example 2:
+    Input: N = 3, dislikes = [[1,2],[1,3],[2,3]]
+    Output: false
+
+    Example 3:
+    Input: N = 5, dislikes = [[1,2],[2,3],[3,4],[4,5],[1,5]]
+    Output: false
+    
+    Note:
+        * 1 <= N <= 2000
+        * 0 <= dislikes.length <= 10000
+        * 1 <= dislikes[i][j] <= N
+        * dislikes[i][0] < dislikes[i][1]
+        * There does not exist i != j for which dislikes[i] == dislikes[j].
+"""
+
+class Solution:
+    def possibleBipartition(self, N: int, dislikes: List[List[int]]) -> bool:      
+        graph = collections.defaultdict(list)
+        
+        visited = set()
+        
+        colors = {}
+        
+        for u, v in dislikes:
+            graph[u].append(v)
+            graph[v].append(u)
+        
+        def dfs(node, color):
+            visited.add(node)
+            
+            if node in colors:
+                return colors[node] == color
+            
+            colors[node] = color
+            
+            ifBipartite = True
+            
+            for v in graph[node]:
+                if v not in visited:
+                    ifBipartite &= dfs(v, -color)
+                if colors[v] == colors[node]:
+                    return False
+                
+            return ifBipartite
+        
+        
+        
+        for node in range(1, N+1):
+            if node not in visited:
+                if not dfs(node, 1):
+                    return False
+        
+        return True
